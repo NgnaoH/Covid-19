dc14d115739b329258179a04a5675c3687394cbf
NgnaoH <66554255+NgnaoH@users.noreply.github.com>
2020-12-08 00:42:42 +0700

    aaa

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+    <v-content>
+      <Home />
+    </v-content>
+  </v-app>
+</template>
+<script>
+import Home from "./components/Home";
+
+export default {
+  name: "App",
+  components: {
+    Home
+  },
+};
+</script>
+<style>
+#app {
+  background: rgb(244, 246, 248) none repeat scroll 0% 0% !important;
+  font-family: "Roboto", sans-serif;
+}
+.tiya {
+  text-decoration: none;
+  color: rgb(76, 175, 80) !important;
+}
+</style>
